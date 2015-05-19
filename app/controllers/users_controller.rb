84f8d3b4f6e871bdf781cef1a6e9325b6bb19521
Tom Ballingall <tom.ballingall@gmail.com>
2015-05-19 17:26:32 -0400

    create new account

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,21 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+
   def create
+  @user = User.new(user_params)
+  if @user.save
+    session[:user_id] = @user.id
+    redirect_to root_url, notice: "Thank you for signing up!"
+  else
+    render "new"
   end
+end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password, :password_confirmation)
+  end
+end
 end
