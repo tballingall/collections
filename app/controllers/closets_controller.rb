c6a8ed420d2f4592e0a29bca1737783083c62b0e
Tom Ballingall <tom.ballingall@gmail.com>
2015-06-08 17:29:56 -0400

    Testing nested routes for user closets and avatars

---
+++ app/controllers/closets_controller.rb
@@ -0,0 +1,50 @@
+class ClosetsController < ApplicationController
+
+  def new
+    @closets = Closet.new
+  end
+
+  def index
+    @closets = closet.all.paginate(page: params[:page], per_page: '6')
+  end
+
+  def show
+    @closet = closet.find(params[:id])
+    @photo = @closet.photos.new
+  end
+
+  def create
+    @closet = closet.new(closet_params)
+    if @closet.save
+      session[:closet_id] = @closet.id
+      flash[:success] = 'Closet Created'
+      redirect_to @closet
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @closet.update_attributes(closet_params)
+      flash[:success] = 'Successfully Updated'
+      redirect_to user_path
+    else
+      render 'new'
+    end
+  end
+#   private
+
+#   def find_closet
+#     @closet = closet.find(params[:id])
+#     redirect_to root_path unless @closet == current_closet
+#   end
+
+#   def closet_params
+#     params.require(:closet).permit(:name, :closetname, :email, :password,
+#                                  :password_confirmation)
+#   end
+# end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   resources :sessions, only: [:new, :create, :destroy]
-  resources :users, except: :destroy
-  get 'signup', to: 'users#new', as: 'signup'
+  resources :users, except: :destroy, shallow: true do
+    resources :closets, only: [:new, :create, :index]
+    resources :photos, only: [:new, :create, :index]
+  end
+
+   get 'signup', to: 'users#new', as: 'signup'
   get 'login', to: 'sessions#new', as: 'login'
   get 'logout', to: 'sessions#destroy', as: 'logout'
   root 'static_pages#home'
-  resources :photos, only: [:new, :create, :index]
-  end
+end
